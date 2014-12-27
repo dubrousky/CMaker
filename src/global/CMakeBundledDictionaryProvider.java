b2fc67e82beb8e501c3c9c9f045a3824aeeca16a
Aliaksandr Dubrouski <dubrousky@users.noreply.github.com>
2014-12-27 22:14:28 +0300

    Adds cmake keywords to the spellcheck dictionary

---
+++ src/global/CMakeBundledDictionaryProvider.java
@@ -0,0 +1,13 @@
+package global;
+
+/**
+ * Created by alex on 12/27/14.
+ */
+import com.intellij.spellchecker.BundledDictionaryProvider;
+
+public class CMakeBundledDictionaryProvider implements BundledDictionaryProvider {
+    @Override
+    public String[] getBundledDictionaries() {
+        return new String[]{"/dictionary/cmake.dic"};
+    }
+}
